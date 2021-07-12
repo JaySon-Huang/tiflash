5dfe2f4818edd522d15e918c3e2458a31082f438
JaySon-Huang <jayson.hjs@gmail.com>
2021-07-12 20:42:04 +0800

    reading writable files
    Signed-off-by: JaySon-Huang <jayson.hjs@gmail.com>

--- dbms/src/Storages/Page/tests/gtest_legacy_compactor.cpp
+++ dbms/src/Storages/Page/tests/gtest_legacy_compactor.cpp
@@ -12,8 +12,8 @@
 #include <Storages/Page/PageFile.h>
 #include <Storages/Page/WriteBatch.h>
 #include <Storages/PathPool.h>
-#include <common/logger_useful.h>
 #include <TestUtils/TiFlashTestBasic.h>
+#include <common/logger_useful.h>
 
 #define private public
 #include <Storages/Page/gc/LegacyCompactor.h>
@@ -188,9 +188,12 @@ try
 
     PageStorage::MetaMergingQueue mergine_queue;
     {
-        auto reader = page_file.createMetaMergingReader();
-        reader->moveNext();
-        mergine_queue.push(std::move(reader));
+        if (auto reader = PageFile::MetaMergingReader::createFrom(page_file); //
+            reader->hasNext())
+        {
+            reader->moveNext();
+            mergine_queue.push(std::move(reader));
+        }
     }
 
     DB::PageStorage::StatisticsInfo   debug_info;
