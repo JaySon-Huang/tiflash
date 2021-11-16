0f1bb65d79f6be1126e92c484569054b4f729b6d
JaySon-Huang <jayson.hjs@gmail.com>
2021-11-16 11:59:05 +0800

    Temp save
    Signed-off-by: JaySon-Huang <jayson.hjs@gmail.com>

--- cmake/find/unwind.cmake
+++ cmake/find/unwind.cmake
@@ -1,10 +1,15 @@
 option (USE_UNWIND "Enable libunwind (better stacktraces)" ON)
+option (USE_INTERNAL_MEMCPY "Use internal libunwind" OFF)
 
 if (USE_UNWIND)
-    add_subdirectory(contrib/libunwind-cmake)
-    set (UNWIND_LIBRARIES unwind)
-    set (EXCEPTION_HANDLING_LIBRARY ${UNWIND_LIBRARIES})
+    if (USE_INTERNAL_MEMCPY)
+        add_subdirectory(contrib/libunwind-cmake)
+        set (UNWIND_LIBRARIES unwind)
+    else()
+        find_library(UNWIND unwind REQUIRED)
+    endif()
 
+    set (EXCEPTION_HANDLING_LIBRARY ${UNWIND_LIBRARIES})
     message (STATUS "Using libunwind: ${UNWIND_LIBRARIES}")
 else ()
     set (EXCEPTION_HANDLING_LIBRARY gcc_eh)
